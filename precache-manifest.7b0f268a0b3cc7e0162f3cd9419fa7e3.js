46ac3341fd1a2a4f08bf34a3ddadacfb1716a2ae
unknown <smitgor1620@gmail.com>
2020-06-17 11:12:57 +0530

    Updates

---
+++ precache-manifest.7b0f268a0b3cc7e0162f3cd9419fa7e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6273c6693dab0cec57af212de6a97fa2",
+    "url": "/omtimistic/index.html"
+  },
+  {
+    "revision": "73ba5e9f2c073ffcebe1",
+    "url": "/omtimistic/static/css/main.373f0558.chunk.css"
+  },
+  {
+    "revision": "e105f88016b1b10e0557",
+    "url": "/omtimistic/static/js/2.74768a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/omtimistic/static/js/2.74768a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ba5e9f2c073ffcebe1",
+    "url": "/omtimistic/static/js/main.a3d0a7fe.chunk.js"
+  },
+  {
+    "revision": "2b231fd0736d790db8e7",
+    "url": "/omtimistic/static/js/runtime-main.b565e820.js"
+  }
+]);
